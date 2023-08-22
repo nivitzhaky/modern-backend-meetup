7a69f84f5356b0aca04c94f52ac8d4bebee827ec
HALLE <hallel14193@gmail.com>
2023-08-22 17:08:12 +0300

    with docker compose

---
+++ src/main/java/com/handson/backend/ModernBackendApplication.java
@@ -0,0 +1,13 @@
+package com.handson.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernBackendApplication.class, args);
+    }
+
+}
